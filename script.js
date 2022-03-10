3a0d9191e957edddd5cc586eb81648fc6006ee4b
JRodriguez-Repo <97987743+JRodriguez-Repo@users.noreply.github.com>
2022-03-10 15:02:12 -0500

    Update README.md

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ const currency1 = document.getElementById('currency1');
 const currency2 = document.getElementById('currency2');
 const amount1 = document.getElementById('amount1');
 const amount2 = document.getElementById('amount2');
-const rate = document.getElementById('rate');
+const swap_rate = document.getElementById('rate');
 const swapBtn = document.getElementById('swap');
 const updatedLast = document.getElementById('last_updated');
 const updateNext = document.getElementById('next_update');
-
 
 
 //get currency rate and update DOM
@@ -15,30 +14,35 @@ function calculate() {
     const input1 = currency1.value;
     const input2 = currency2.value;
     
-    fetch(`https://v6.exchangerate-api.com/v6/47337334b42d7c1572f3bc70/latest/${input1}`)
+    //fetch data from api source
+    fetch(`https://open.er-api.com/v6/latest/${input1}`)
     .then(response => response.json())
     .then((data) => {
         
             console.log(data);
         
-        const conversion_rate = data.conversion_rates[input2];
-        rate.innerHTML = `1 ${input1} = ${conversion_rate} ${input2}`;
+        const conversion_rate = data.rates[input2];
+        swap_rate.innerHTML = `1 ${input1} = ${conversion_rate} ${input2}`;
         amount2.value = (amount1.value * conversion_rate).toFixed(2);
 
         
-        const last_update = data.time_last_update_utc;
-        const next_update = data.time_next_update_utc;
-        updatedLast.innerHTML = `Last update @ ${last_update}`;
-        updateNext.innerHTML = `Next update @ ${next_update}`;
+        let last_update = data.time_last_update_utc;
+        let next_update = data.time_next_update_utc;
+        let formatted_last_update = last_update.slice(0, 17);
+        let formatted_next_update = next_update.slice(0,17);
+
+        updatedLast.innerHTML = `Last update @ ${formatted_last_update}`;
+        updateNext.innerHTML = `Next update @ ${formatted_next_update}`;
 
     });
          
 }
-
 currency1.addEventListener('change', calculate);
 currency2.addEventListener('change', calculate);
 amount1.addEventListener('input', calculate);
 amount2.addEventListener('input', calculate);
+
+//swap values for exchange rate calculation
 swapBtn.addEventListener('click', () => {
 
 
